4d9d343a500b8a135830c171b28217377f923d02
Louis Bettens <lbe@open-net.ch>
2016-09-08 10:06:43 +0200

    git start

---
+++ __openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Open Net Sarl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Open-Net Customization for Open Net',
+    'summary': 'Open Net Customization for Open Net',
+    'category': 'Open Net customizations',
+    'author': "Open Net Sàrl",
+    'depends': [
+        'account',
+        'sale_layout',
+        'report'
+    ],
+    'version': '9.0.1.0.2',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+        'views/invoice_report.xml',
+        'views/external_header.xml'
+    ],
+    'installable': True
+}
