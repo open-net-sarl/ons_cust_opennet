35b8cb68e584531fd0278d1b9f753ce87dcf889d
Odoo <odoo@www.open-net.ch>
2017-09-27 10:16:10 +0200

    Modified and finished the info mail of the stock moves

--- models/stock_move.py
+++ models/stock_move.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
+import datetime
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -18,6 +19,15 @@ class StockMove(models.Model):
     def _cron_info_person_in_charge(self):
         records = self.search([])
         for record in records:
-            if record.ons_cust_person_in_charge and record.state in ['assigned', 'confirmed', 'waiting']:
+            date_expected = fields.Date.from_string(record.date_expected)
+            if record.ons_cust_person_in_charge and \
+               record.state in ['assigned', 'confirmed', 'waiting'] and \
+               date_expected <= datetime.datetime.today().date():
                 mail_tmp = self.env.ref('ons_cust_opennet.email_template_opennet_info_cron')
                 mail_tmp.send_mail(record.id)
+
+    @api.multi
+    def info_for_mail_person_in_charge(self):
+        action = self.env.ref('stock.stock_move_action') or False
+        menu = self.env.ref('stock.menu_stock_root') or False
+        return {'action': action,'menu': menu}
