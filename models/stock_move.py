7b7864e39546a15d4ea4bf79fce732a74757a810
Odoo <odoo@www.open-net.ch>
2018-01-19 11:10:44 +0100

    Stock moves: format date correctly If no one is in charge of the stock_move then put jae as person in charge

--- models/stock_move.py
+++ models/stock_move.py
@@ -17,19 +17,27 @@ class StockMove(models.Model):
 
     ons_order_id = fields.Many2one(related="procurement_id.sale_line_id.order_id", string="Sale Order")
 
+    contract_id = fields.Many2one('eagle.contract', string='File')
+
     @api.model
     def _cron_info_person_in_charge(self):
         records = self.search([])
+        jae = self.env['res.partner'].search([('ons_cust_email_two', '=', 'jae@open-net.ch')], limit=1)
         for record in records:
             date_expected = fields.Date.from_string(record.date_expected)
-            if record.ons_cust_person_in_charge and \
-               record.state in ['assigned', 'confirmed', 'waiting'] and \
+            if record.state in ['assigned', 'confirmed', 'waiting'] and \
                date_expected <= datetime.datetime.today().date():
-                mail_tmp = self.env.ref('ons_cust_opennet.email_template_opennet_info_cron')
-                mail_tmp.send_mail(record.id)
+                if record.ons_cust_person_in_charge:
+                    mail_tmp = self.env.ref('ons_cust_opennet.email_template_opennet_info_cron')
+                    mail_tmp.send_mail(record.id)
+                else:
+                    mail_tmp = self.env.ref('ons_cust_opennet.email_template_opennet_info_cron')
+                    record.ons_cust_person_in_charge = jae
+                    mail_tmp.send_mail(record.id)
 
     @api.multi
     def info_for_mail_person_in_charge(self):
         action = self.env.ref('stock.stock_move_action') or False
         menu = self.env.ref('stock.menu_stock_root') or False
-        return {'action': action,'menu': menu}
+        date_expected = fields.Date.from_string(self.date_expected)
+        return {'action': action,'menu': menu, 'date_expected': date_expected}
