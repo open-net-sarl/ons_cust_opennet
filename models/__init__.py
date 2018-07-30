0c586341da9a56be3267e7e77e607923804cd7e1
Remy Zulauff <rzu@open-net.ch>
2018-07-30 14:44:53 +0200

    migration: pricing

--- models/__init__.py
+++ models/__init__.py
@@ -2,22 +2,22 @@
 # © 2016 Coninckx David (Open Net Sarl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import account_reports
-from . import crm_lead
-from . import invoice
-from . import res_partner
-from . import mass_mailing
-from . import ons_cust_customer_config
-from . import eagle_contract
-from . import stock_move
-from . import res_partner
+# from . import account_reports
+# from . import crm_lead
+# from . import invoice
+# from . import res_partner
+# from . import mass_mailing
+# from . import ons_cust_customer_config
+# from . import eagle_contract
+# from . import stock_move
+# from . import res_partner
 from . import ons_functional_area
 from . import ons_pricing_option
 from . import ons_hosting
-from . import project_issue
+# from . import project_issue
 from . import ons_ir_module
 from . import ons_user_pricing
 from . import ons_pricing_client
-from . import crossovered_budget
-from . import event
-from . import project_task
+# from . import crossovered_budget
+# from . import event
+# from . import project_task
